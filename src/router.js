f61f1891bfb011e39daaaff5ced2e0a6b9515ddf
Xavier Julien <xjulien@octo.com>
2018-07-01 13:41:03 +0200

    Change baseUrl for production

--- src/router.js
+++ src/router.js
@@ -6,6 +6,9 @@ import Status from '@/components/Status.vue'
 Vue.use(Router)
 
 export default new Router({
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/mccb_ng/'
+    : '/',
   routes: [
     {
       path: '/',
