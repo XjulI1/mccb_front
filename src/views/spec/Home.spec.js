def6176e2dd894b83ac4628465dc1db9df60effc
Xavier Julien <xjulien@octo.com>
2019-02-03 23:06:26 +0100

    Fix tests

--- src/views/spec/Home.spec.js
+++ src/views/spec/Home.spec.js
@@ -3,21 +3,12 @@ import { createLocalVue, shallowMount } from '@vue/test-utils'
 import Router from 'vue-router'
 
 import Home from '@/views/Home'
-import Navbar from '@/components/Navbar'
 
 const localVue = createLocalVue()
 
 localVue.use(Router)
 
 describe('Home.vue', () => {
-  test('should render correct contents', () => {
-    const wrapper = shallowMount(Home, {
-      localVue
-    })
-
-    expect(wrapper.findAll(Navbar).length).toBe(1)
-  })
-
   test('should match snapshot', () => {
     const wrapper = shallowMount(Home, {
       localVue
